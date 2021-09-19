1c4edc465ed4b6b32a6a16132ff5b2f73738d19f
stconway0 <stconway@iu.edu>
2021-09-19 01:35:23 -0400

    HW & Bug Fixes + Index Page Redesign
    The following commit addresses much of the issues encountered in HW1 and 2a, and updates the overall structure of the page.
    
    Changes made:
    HW1:
    > Declared and ID element in the “Book Table” component of the project.  The CSS now uses the “bookTable”ID tag to perform table design.
    
    HW2a:
    > Folder name change to fit the class format
    
    Other Fixes:
    > Updated pages to use Bootstrap 5
    > Fixed navbar to work with Bootstrap 5
    > Redesigned the index.html page to be in compliance with class requirements & Bootstrap 5 standards
    > Added index.js file for future iterations of the website
    > Consolidated the CSS sheet
    > Removed <style> tags
    > Removed the user <br> tags
    > Removed <center> tags
    > Removed footer for visual design improvement & lack of need

---
+++ app/public/js/index.js
@@ -0,0 +1,21 @@
+
+
+
+/*
+
+
+cont Person = {
+    data() {
+        return {
+
+        }
+    }
+}
+
+
+*/
+
+
+
+/* Assigning random person data to a div element with id #personInfo */
+Vue.createApp(Person).mount("#personInfo");
