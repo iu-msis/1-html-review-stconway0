9d41ef34470f26a3c81a7b2efed05638473f077e
stconway0 <stconway@iu.edu>
2021-10-20 11:39:48 -0400

    Updated From to Add Books to DB
    > Added functionality to add books to the book DB
    > Redesigned the "Add Book Form"
    > Added create.php to update the books table
    > Updated site.css

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,19 +1,17 @@
 const BooksApp = {
     data() {
       return {
-        books: []
+        books: [],
+        bookForm: {}
       }
     },
     computed: {},
     methods: {
-        prettyData(d) {
-            return dayjs(d)
-            .format('D MMM YYYY')
-        },
         prettyDollar(n) {
             const d = new Intl.NumberFormat("en-US").format(n);
             return "$ " + d;
         },
+
         /* Fetching data for books.html */
         fetchBooksData() {
             fetch('/api/books/')
@@ -26,6 +24,29 @@ const BooksApp = {
                 console.error(err);
             })
         },
+
+        postNewBook(evt) {
+            fetch('api/books/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+          },
+
+        handleResetEdit() {
+            this.bookForm = {};
+        }
     },
     created() {
         this.fetchBooksData();
